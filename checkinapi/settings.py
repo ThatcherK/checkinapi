b380fd2ba28c4cc1903a181835391c56faecd10a
ThatcherK <kaliithatmau@gmail.com>
2020-11-27 00:40:02 +0300

    Add cors settings

--- checkinapi/settings.py
+++ checkinapi/settings.py
@@ -41,18 +41,25 @@ INSTALLED_APPS = [
     'rest_framework',
     'checkin',
     'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:9000"
+]
 REST_FRAMEWORK={
     'DEFAULT AUTHENTICATION_CLASSES':(
         'authentication.authbackend.JWTAuthentication',
